4948d98f5e988b590e2738e3801ae7839f49faaf
Jean Philippe Carlens <=>
2022-03-06 21:40:09 +0100

    refactor

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
-        console.log(`Kurome s'est connectée en tant que ${client.user.tag}`);
+        console.log(`Le bot s'est connectée en tant que ${client.user.tag}`);
     },
 };
